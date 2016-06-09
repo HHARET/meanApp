9562c936a9c6dfa5574ef9747d3da7b527741c0c
Tarek <tarek_s_a@yahoo.com>
2016-06-09 10:42:38 -0430

    routing using express

---
+++ src/app.js
@@ -0,0 +1,12 @@
+'use strict';
+var express = require('express');
+var router=require('./api');
+
+var app= express();
+
+app.use('/',express.static('public'));
+
+app.use('/api',router);
+app.listen(3000,function(){
+  console.log("the server is runnning tarek")
+});
