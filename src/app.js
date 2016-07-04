aa3a0fc970f5adaa83a635b7c673795cd4e85e9b
Tarek <tarek_s_a@yahoo.com>
2016-07-03 20:15:17 -0430

    added the port env

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@
 var express = require('express');
 var parser = require('body-parser');
 var router=require('./api');
-app.set('port', (process.env.PORT || 5000));
+
 var app= express();
 require('./database');
 require('./seed');
+
+app.set('port',(process.env.PORT || 5000));
+
 app.use('/',express.static('public'));
 //this is the body-parser installed because express
 //does not have a body-parser by default
 app.use(parser.json());
 
 app.use('/api',router);
-app.listen(5000,function(){
+app.listen(app.get('port'),function(){
   console.log("the server is runnning tarek")
 });
