f8afccdd64651a79ff261e700c717ee485297fe3
Tarek <tarek_s_a@yahoo.com>
2016-08-12 18:17:56 -0430

    added config variable for mongoose

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 'use strict';
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/mean-todo',function(err){
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mean-todo',function(err){
   if (err){console.log("error en database")}
   else{console.log("successfully connected to mongo");} {
 
